325fc67277eee6d7ffd5cd9825b78e959107c80b
Kevin Martinez <kemart1230@gmail.com>
2025-10-08 22:58:02 -0600

    implement workflow

--- s8/knexfile.ts
+++ s8/knexfile.ts
@@ -17,7 +17,7 @@ export default {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      database: process.env.DB,
       migrations: {
         tableName: 'migrations'
       }
